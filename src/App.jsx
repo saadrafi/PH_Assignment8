ad8bcc2da0691ac716f413316c11086d0dcd1003
saadrafi <saadrafi81@gmail.com>
2023-03-30 23:11:35 +0600

    Header section design finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1 className='text-3xl font-bold'>HEllo world</h1>
+    <div className='lg:w-[80%] mx-auto'>
+        <Header />
+    </div>
   )
 }
 
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import avatar from '../../assets/imges/profile.jpg'
+
+const Header = () => {
+    return (
+        <div className='p-10 flex justify-between items-center border-b-2'>
+            <h1 className='text-4xl font-bold text-[#111111]'>Knowledge Cafe</h1>
+            <div className='w-14 h-14 '> <img className='rounded-full'  src={avatar} alt="" /></div>
+            
+        </div>
+    );
+};
+
+export default Header;
